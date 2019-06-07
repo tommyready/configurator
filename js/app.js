44ccbe9d3acf2fe64a52225b548cf760598a453a
Tommy Ready <tready@humna-element.com>
2019-06-07 02:01:39 -0400

    playing with fabric js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 ;(function(){
 
-  var unicornSVG = SVG('svg-body').size(500,500);
+  /*let unicornSVG = SVG('svg-body').size(500,500);
+
+
 
   jQuery.get('unicorn.svg', function(rawSvg) {
     const svgContent = jQuery('svg', rawSvg);
     unicornSVG.svg(svgContent.html());
   }, 'xml');
+   */
 
+
+  const canvas = new fabric.Canvas('canvas');
+  fabric.loadSVGFromURL('unicorn.svg', function(objects, options) {
+    const obj = fabric.util.groupSVGElements(objects, options);
+    canvas.add(obj).renderAll();
+  });
   
 })(jQuery);
-  
-  
-  
-  updatePathFill = function(fillColor, targetPath)
-  {
-    jQuery('#'+targetPath).attr('fill', '#'+fillColor);
-  }
+
+
+
+
+updatePathFill = function(fillColor, targetPath) {
+  jQuery('#'+targetPath).attr('fill', '#'+fillColor);
+}
